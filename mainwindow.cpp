947b394280c4549a93ac28af0aec28ac8bb21ff2
Omar <omarkhial74@gmail.com>
2022-02-23 03:05:54 +0100

    exercice is mostly finished
    I wonder if i should make Other column insted of printing all the others

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,9 +17,11 @@ MainWindow::~MainWindow()
     delete manager;
 }
 
+////////////////////////////////////
+//// ------------------ Http Request
+////////////////////////////////////
 
-//// ------------------ REQUEST AND CRITERIA SELECTIONS
-
+// Do updates and store data when reply has arrived
 void MainWindow::handleReply(QNetworkReply *reply) {
     if (reply->error()) {
         qDebug() << reply->errorString();
@@ -32,10 +34,12 @@ void MainWindow::handleReply(QNetworkReply *reply) {
 
     this->data = doc;
 
+
     fillTable();
+    updateLoadBtnText("Données prêtes");
 }
 
-
+// make the request with the given parameters
 void MainWindow::makeRequest(QString sDate, QString eDate, QString nbRows)
 {
     QString url  = "https://data.drees.solidarites-sante.gouv.fr";
@@ -48,20 +52,26 @@ void MainWindow::makeRequest(QString sDate, QString eDate, QString nbRows)
 
 }
 
-void MainWindow::on_loadBtn_clicked()
+////////////////////////////////////
+//// ------------------ Utils
+////////////////////////////////////
+
+
+void MainWindow::updateLoadBtnText(QString text)
 {
-    const QString dateFormat = "yyyy-MM-dd";
-
-    QString sDate  = ui->start_date->date().toString(dateFormat);
-    QString eDate  = ui->end_date->date().toString(dateFormat);
-    QString nbRows = ui->nb_rows->text() == "" ? "10" : ui->nb_rows->text();
-
-    makeRequest(sDate, eDate, nbRows);
-
+    ui->loadBtn->setText(text);
 }
 
-
-//// ------------------ TABLE VIEW
+// store the biggest effectif from all fields
+void calcEffectif(std::vector<double>& effectif, QJsonValue fields, int index)
+{
+    double oldEff = effectif.at(index);
+    double newEff = fields["effectif"].toDouble();
+    if(oldEff == 0 || oldEff < newEff)
+    {
+        effectif.at(index) = newEff;
+    }
+}
 
 int getIndex(std::vector<QString> v, QString k)
 {
@@ -74,10 +84,25 @@ int getIndex(std::vector<QString> v, QString k)
     return it - v.begin();
 }
 
+// Return the index of the field to update the column
+int MainWindow::indexOfStatut(QJsonValue fields)
+{
 
+    QString statut = fields["vac_statut"].toString();
+
+    return getIndex(statusIndex, statut);
+}
+
+////////////////////////////////////
+//// ------------------ TABLE VIEW
+////////////////////////////////////
+
+// Create Headers for the TableView, with the given array
 void MainWindow::createTableHeaders(QJsonArray status, std::vector<QString> rowsHeaderVal)
 {
 
+    // I store the index of status in a tab
+    // to be able to retreive the status index for inserting data
     for (int i = 0; i < status.size() ; i++ ) {
         QJsonValue s = status[i];
         QString sName = s["name"].toString();
@@ -90,31 +115,67 @@ void MainWindow::createTableHeaders(QJsonArray status, std::vector<QString> rows
         verticalHeader.append(rowsHeaderVal[i]);
     }
 
+    // set the model to the table view
     model.index(1,1,model.index(0,0));
     model.setHorizontalHeaderLabels(horizontalHeader);
     model.setVerticalHeaderLabels(verticalHeader);
 
     ui->tableResults->setModel(&model);
 
+    // we want to do this only one time
     filled = true;
+}
+
+
+// Display percentage for each cell
+void MainWindow::showPercentage(std::vector<double> effectif, std::vector<QString> rowsHeaderVal)
+{
+
+    for(int i = 0; i < ui->tableResults->model()->columnCount(); i++)
+    {
+        for(size_t j = 0; j < rowsHeaderVal.size(); j++ )
+        {
+            QModelIndex qIndex = ui->tableResults->model()->index(j, i);
+            QVariant oldV = ui->tableResults->model()->data(qIndex);
+            qDebug() << oldV;
+
+            // we made percentage only if the value is valid
+            if(oldV.isValid() && oldV.toString() != "NC")
+            {
+                QString percent = QString::number( (oldV.toDouble() * 100 ) / effectif.at(i), 'f', 3);
+                QString valAndPercent = oldV.toString() + " / " + percent + " %";
+                ui->tableResults->model()->setData(qIndex, valAndPercent);
+            }
+            else
+            {
+                ui->tableResults->model()->setData(qIndex, "NC");
+            }
+        }
+    }
+}
+
+// clear all cells of table
+void MainWindow::clearCells(std::vector<QString> rowsHeaderVal)
+{
+    for(int i = 0; i < ui->tableResults->model()->columnCount(); i++)
+    {
+        for(size_t j = 0; j < rowsHeaderVal.size(); j++ )
+        {
+            QModelIndex qIndex = ui->tableResults->model()->index(j, i);
+            qDebug() << i << j << ui->tableResults->model()->clearItemData(qIndex);
+        }
+    }
 }
 
 void MainWindow::fillTable()
 {
+    // Get all status and records and store it
     QJsonArray status = this->data["facet_groups"][1]["facets"].toArray();
     std::vector<QString> rowsHeaderVal = {"hc_pcr", "sc_pcr", "dc_pcr"};
+    QJsonArray records = this->data["records"].toArray();
 
     if(!filled)
-    {
         createTableHeaders(status, rowsHeaderVal);
-    }else
-    {
-        /// RESET TABLE
-        ui->tableResults->model()->removeColumns(0, status.size());
-        ui->tableResults->model()->removeRows(0, 3);
-    }
-
-    QJsonArray records = this->data["records"].toArray();
 
     std::vector<double> effectif = std::vector<double>(statusIndex.size());
     std::fill(effectif.begin(), effectif.end(), 0);
@@ -123,57 +184,80 @@ void MainWindow::fillTable()
     {
         QJsonValue r = records[i];
         QJsonValue fields = r["fields"];
-        QString statut = fields["vac_statut"].toString();
-        int index = getIndex(statusIndex, statut);
 
-        // calcul du pourcentage
-        double oldEff = effectif.at(index);
-        double newEff = fields["effectif"].toDouble();
-        if(oldEff == 0 || oldEff < newEff)
-        {
-            effectif.at(index) = newEff;
-        }
+        // we get the corresponding index of the status.
+        int index = indexOfStatut(fields);
 
+        calcEffectif(effectif, fields, index);
+
+        // if the column exist
         if(index != -1)
         {
-                for(size_t j = 0; j < rowsHeaderVal.size(); j++ )
+            for(size_t j = 0; j < rowsHeaderVal.size(); j++ )
+            {
+                // we getting the index and the old value
+                QModelIndex qIndex = ui->tableResults->model()->index(j, index);
+                QVariant oldV = ui->tableResults->model()->data(qIndex);
+
+                if(oldV != "" && oldV.isValid())
                 {
-
-                    QModelIndex qIndex = ui->tableResults->model()->index(j, index);
-                    QVariant oldV = ui->tableResults->model()->data(qIndex);
-
-                    if(oldV != "")
-                    {
-                        double count = (fields[rowsHeaderVal[j]].toDouble() + oldV.toDouble());
-                        QString newV = QString::number(count);
-                        ui->tableResults->model()->setData(qIndex, newV);
-                        qDebug() <<  effectif.at(index);
-                        qDebug() << (count * 100 ) / effectif.at(index) << " %";
-                    }
+                    // making sum if the old value is valid
+                    double count = (fields[rowsHeaderVal[j]].toDouble() + oldV.toDouble());
+                    QString newV = QString::number(count);
+                    ui->tableResults->model()->setData(qIndex, newV);
                 }
-        }else
-        {
-            qDebug() << "error values are in the wrong format.";
+            }
         }
     }
 
-    for(int i = 0; i < ui->tableResults->model()->columnCount(); i++)
-    {
-            auto hItem = model.horizontalHeaderItem(i);
-            int index = getIndex(statusIndex, hItem->text());
-            for(size_t j = 0; j < rowsHeaderVal.size(); j++ )
-            {
-                QModelIndex qIndex = ui->tableResults->model()->index(j, index);
-                QVariant oldV = ui->tableResults->model()->data(qIndex);
-                QString percent = QString::number( (oldV.toDouble() * 100 ) / effectif.at(index), 'f', 3);
-                QString valAndPercent = oldV.toString() + " / " + percent + " %";
-                ui->tableResults->model()->setData(qIndex, valAndPercent);
-            }
-    }
-
-
+    // Printing percentage after the all values are set
+    showPercentage(effectif, rowsHeaderVal);
 
     ui->tableResults->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
 }
+
+
+////////////////////////////////////
+//// ------------------ UI Events
+////////////////////////////////////
+
+// We made a request with the user data onClick.
+
+void MainWindow::on_loadBtn_clicked()
+{
+    const QString dateFormat = "yyyy-MM-dd";
+
+    QString sDate  = ui->start_date->date().toString(dateFormat);
+    QString eDate  = ui->end_date->date().toString(dateFormat);
+    QString nbRows = ui->nb_rows->text() == "" ? "10" : ui->nb_rows->text();
+
+    makeRequest(sDate, eDate, nbRows);
+    updateLoadBtnText("Chargement en cours...");
+}
+
+
+// When any field is modified, we update the text button
+// For the user to know that i can have new set of values
+
+void MainWindow::on_nb_rows_textChanged(const QString &arg1)
+{
+    (void) arg1;
+    updateLoadBtnText("Charger");
+}
+
+
+void MainWindow::on_end_date_dateChanged(const QDate &date)
+{
+    (void) date;
+    updateLoadBtnText("Charger");
+}
+
+
+void MainWindow::on_start_date_dateChanged(const QDate &date)
+{
+    (void) date;
+    updateLoadBtnText("Charger");
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -16,9 +16,6 @@
 #include <QHeaderView>
 
 
-
-
-
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -30,13 +27,21 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    void makeRequest(QString sDate, QString eDate, QString nbRows);
 
+    // Request
+    void makeRequest(QString sDate, QString eDate, QString nbRows);
+    // Table
     void createTableHeaders(QJsonArray status, std::vector<QString> rowsHeaderVal);
     void fillTable();
+    void clearCells(std::vector<QString> rowsHeaderVal);
+    void showPercentage(std::vector<double> effectif, std::vector<QString> rowsHeaderVal);
+    // Utils
+    int  indexOfStatut(QJsonValue fields);
+    void updateLoadBtnText(QString text);
 
 private:
     Ui::MainWindow *ui;
+
     // Network Objects
     QNetworkAccessManager *manager;
     QNetworkRequest request;
@@ -46,8 +51,9 @@ private:
     QModelIndex modelIndex;
     QStringList horizontalHeader;
     QStringList verticalHeader;
-    bool filled = false;
-    std::vector<QString> statusIndex;
+
+    bool filled = false;                // init bool for table view
+    std::vector<QString> statusIndex;   // vector to store order of status in tab
 
     // Stored data
     QJsonDocument data;
@@ -58,6 +64,9 @@ private slots:
     void handleReply(QNetworkReply *reply);
     void on_loadBtn_clicked();
 
+    void on_nb_rows_textChanged(const QString &arg1);
+    void on_end_date_dateChanged(const QDate &date);
+    void on_start_date_dateChanged(const QDate &date);
 };
 
 #endif // MAINWINDOW_H
